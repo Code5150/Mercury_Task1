17bcbd536e9e153600ae8357e77d1c10df6f42fe
Владислав <163vsk@gmail.com>
2020-03-05 21:04:56 +0400

    Экран с иконкой, появляющийся на заданное время и сделанный на корутине

---
+++ app/src/main/java/com/example/mercury_task1/ColorListActivity.kt
@@ -0,0 +1,50 @@
+package com.example.mercury_task1
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+
+class ColorListActivity : AppCompatActivity() {
+    private lateinit var coloredItemsRecyclerView: RecyclerView
+
+    private val ELEMENTS_NUM: Int = 50
+    private val COLORS_NUM: Int = 8
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_list)
+        coloredItemsRecyclerView = findViewById(R.id.coloredItemsRecyclerView)
+        coloredItemsRecyclerView.layoutManager = LinearLayoutManager(this)
+        coloredItemsRecyclerView.setHasFixedSize(true)
+        val itemsList = fillAdapterItems()
+        coloredItemsRecyclerView.adapter = RecyclerAdapter(itemsList) { str ->
+            Snackbar.make(coloredItemsRecyclerView, getString(R.string.item_clicked, str), Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun fillAdapterItems(): ArrayList<ColorItem>{
+        val list = ArrayList<ColorItem>()
+        for (i in 0 until ELEMENTS_NUM){
+            val c = when (i % COLORS_NUM){
+                0 -> Color.RED
+                1 -> Color.rgb(255, 165, 0)
+                2 -> Color.YELLOW
+                3 -> Color.GREEN
+                4 -> Color.rgb(66, 170, 255)
+                5 -> Color.BLUE
+                6 -> Color.rgb(139, 0 ,255)
+                else -> 0
+            }
+            var visible: Boolean = when (i % COLORS_NUM) {
+                7 -> false
+                else -> true
+            }
+            val item = ColorItem(c, getString(R.string.item_text, i+1), visible)
+            list += item
+        }
+        return list
+    }
+}
