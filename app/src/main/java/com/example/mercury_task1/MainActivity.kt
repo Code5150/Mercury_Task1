90a2606729aead0bfa3fb6f633c0991a2508d74e
Владислав <163vsk@gmail.com>
2020-03-11 18:57:13 +0400

    Время до запуска теперь сохраняется в state

--- app/src/main/java/com/example/mercury_task1/MainActivity.kt
+++ app/src/main/java/com/example/mercury_task1/MainActivity.kt
@@ -2,23 +2,38 @@ package com.example.mercury_task1
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+
+val DELAY_TIME: String = "DELAY_TIME"
 
 class MainActivity : AppCompatActivity(), CoroutineScope {
 
     private var localJob: Job = Job()
     override val coroutineContext = Dispatchers.Default + localJob
+    private var delayTime: Long = 2000L
+    private var currentDate: Long = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            delayTime = savedInstanceState.getLong(DELAY_TIME)
+        }
+        currentDate = SystemClock.elapsedRealtime()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        delayTime -= SystemClock.elapsedRealtime() - currentDate
+        outState.putLong(DELAY_TIME, delayTime)
     }
 
     override fun onStart() {
         val intent: Intent = Intent(this, ColorListActivity::class.java)
         localJob = launch {
-            delayedActivityLaunch(2000L, intent)
+            delayedActivityLaunch(intent)
         }
         super.onStart()
     }
@@ -28,8 +43,10 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         super.onStop()
     }
 
-    private suspend fun delayedActivityLaunch(delayTime: Long, intent: Intent) {
-        delay(delayTime)
+    private suspend fun delayedActivityLaunch(intent: Intent) {
+        if (delayTime > 0) {
+            delay(delayTime)
+        }
         startActivity(intent)
         finish()
     }
